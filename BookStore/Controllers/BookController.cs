b23379d74f1c9ccb15c1ca52da019ff4a494a907
mazurewa <mazurewa@users.noreply.github.com>
2017-03-13 09:21:04 +0100

    Upload of the files after the final stage
    Application with adding, deleting and searching of the books

---
+++ BookStore/Controllers/BookController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BookStore.Models;
+
+namespace BookStore.Controllers
+{
+    public class BookController : Controller
+    {
+        private BookDBContext db = new BookDBContext();
+
+        public ActionResult _BookSearch(string query)
+        {
+            var books = GetBooks(query);
+            return PartialView(books);
+        }
+
+        private List<Book> GetBooks(string query)
+        {
+            return db.Books.Where(b => b.Name.Contains(query)).ToList();
+        }
+
+        public ActionResult BargainBook()
+        {
+            var book = GetBargainBook();
+            return PartialView("_BargainBook", book);
+        }
+
+        private Book GetBargainBook()
+        {
+            return db.Books.OrderBy(b => b.Price).First();
+        }
+
+        // GET: Book
+        public ActionResult Index(string bookGenre, string searchString)
+        {
+            var genreList = new List<string>();
+            var genreQuery = db.Books.Select(s => s.Genre);
+            genreList.AddRange(genreQuery.Distinct());
+            ViewBag.BookGenre = new SelectList(genreList);
+
+            IQueryable<Book> books = db.Books;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                books = books.Where(s => s.Name.Contains(searchString));
+            }
+
+            if (!String.IsNullOrEmpty(bookGenre))
+            {
+                books = books.Where(x => x.Genre == bookGenre);
+            }
+
+            return View(books);
+        }
+
+        // GET: Book/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Book book = db.Books.Find(id);
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            return View(book);
+        }
+
+        // GET: Book/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Book/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Name,PubDate,Price,Genre")] Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Books.Add(book);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(book);
+        }
+
+        // GET: Book/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Book book = db.Books.Find(id);
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            return View(book);
+        }
+
+        // POST: Book/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Name,PubDate,Price,Genre")] Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(book).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(book);
+        }
+
+        // GET: Book/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Book book = db.Books.Find(id);
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            return View(book);
+        }
+
+        // POST: Book/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Book book = db.Books.Find(id);
+            db.Books.Remove(book);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ BookStore/Models/BookDBInitializer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace BookStore.Models
+{
+    public class BookDBInitializer : DropCreateDatabaseAlways<BookDBContext>
+    {
+        protected override void Seed(BookDBContext context)
+        {
+            context.Books.Add(new Book()
+            {
+                Name = "Teach C++ in 21 days",
+                PubDate = new DateTime(1995, 1, 1),
+                Price = 29.95m,
+                Genre = "Programming"
+            });
+
+            context.Books.Add(new Book()
+            {
+                Name = "Programming C#",
+                PubDate = new DateTime(2000, 1, 1),
+                Price = 49.95m,
+                Genre = "Programming"
+            });
+
+            context.Books.Add(new Book()
+            {
+                Name = "Pro Windows 8",
+                PubDate = new DateTime(2014, 1, 1),
+                Price = 39.95m,
+                Genre = "Fiction"
+            });
+
+            base.Seed(context);
+        }
+    }
+}
